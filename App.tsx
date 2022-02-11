66580d046f999fc24a8709b290bf1de4dbb49922
Luis Carrion <lcarrionalvarez@gmail.com>
2022-02-11 13:48:50 -0500

    Buttons finished

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.contentButtons}>
+        <Button
+          title='      +1      '
+          onPress={()=> Alert.alert('mas uno :p')}
+        />
+        <View style={styles.contentText}>
+        </View>
+        <Button
+          title='      -1      '
+          onPress={()=> Alert.alert('mas uno :p')}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#33F6FF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  contentButtons: {
+    flex: 0.1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  contentText: {
+    flex:0.2
+  }
 });
